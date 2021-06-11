4ceb744d98a55e644d936473b4b0c26c6ed6b56f
Ridwan Aditya Saputra <riidwanadiitya@gmail.com>
2021-06-11 21:46:48 +0700

    change to HashRouter

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Homepage from './page/Homepage';
 import Undangan from './page/Undangan';
 
